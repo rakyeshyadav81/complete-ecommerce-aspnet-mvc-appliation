971bc28179b1ddf8b874044bd289ccc75fbbf086
rakeshprakash632@gmail.com <rakeshprakash632@gmail.com>
2023-04-10 01:11:26 +0530

    end of actors portions

---
+++ Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()   
+    {
+        //Task<IEnumerable<Actor>> GetAll();
+        //Actor GetById(int id);
+        //void Add(Actor actor);
+        //Actor Update(int id, Actor newActor);
+        //void Delete(int id);
+
+
+    }
+}
